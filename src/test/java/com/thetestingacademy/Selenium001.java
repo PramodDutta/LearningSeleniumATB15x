80ff049a09de3ed232b453d729fd459d180b3474
Pramod Dutta <pramoddutta_96@yahoo.com>
2026-02-20 07:05:33 +0530

    Initial commit

---
+++ src/test/java/com/thetestingacademy/Selenium001.java
@@ -0,0 +1,28 @@
+package com.thetestingacademy;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class Selenium001 {
+    public static void main(String[] args) {
+
+         // Source here ->  (nagivate to the google.com)
+        //  Converted -> JSON Wireprotocol | HTTP Request | api request)
+        //  Browser Driver | Server ( application running on a particulr port)
+        // Browser Driver -> Real Driver(
+
+         // Selenium 3-
+        // Download the driver binary
+        // System.get ---  start the binary
+
+        // Selenium 4
+        // Selenium manager (application)
+        // Selenium will automatically doawloan the drivers and start)
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://app.vwo.com");
+
+
+
+    }
+}
